ff95e494f263283fae241981432e878d9d68e390
Matt Grandy <mgrandy@rogers.com>
2025-12-24 20:21:42 -0500

    Initial Commit

---
+++ DogTennisCatch/DogTennisCatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogTennisCatchApp.swift
+//  DogTennisCatch
+//
+//  Created by Matthew Grandy on 2025-12-24.
+//
+
+import SwiftUI
+
+@main
+struct DogTennisCatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
